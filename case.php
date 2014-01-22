4e2822e5b956f5f697a838604d77e706c9d592c1
carlos eduardo <kadusnt@gmail.com>
2014-01-22 16:09:59 -0200

    Cases.php

---
+++ case.php
@@ -0,0 +1,5 @@
+	<?php include_once ("header.php"); ?>
+		<div class="content-cases">
+	 		<h3>Entre a ordem e o caos, nosso método emerge da escola open-source: agilidade, entregas frequentes e constante adaptação.</h3>
+		</div>
+	<?php include_once ("footer.php"); ?>
