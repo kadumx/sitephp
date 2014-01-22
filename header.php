216949ef3c0a5d9e54301fb415fe75dcac8747d0
carlos eduardo <kadusnt@gmail.com>
2014-01-22 16:16:03 -0200

    Create header

---
+++ header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+	<html lang="pt-br">
+	
+	<head>
+		<meta charset="UTF-8">
+		
+		<meta name=viewport content="width=device-width, initial-scale=1.0, minimum-scale=0.5 maximum-scale=1.0">
+		<!--Arquivo CSS--> 
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<!--Arquivo JavaScript/jQuery--> 
+		<script src=""></script>
+		<script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
+
+
+		<title>Carlos Eduardo - Portfólio</title>
+
+	</head>
+
+	<body>
+		<!--Abrir código--> 
+		<div class="container"> 
+			<div class="header">
+				<h3>Carlos Eduardo</h3>
+			</div>
+			
+			<div class="nav"> 
+				<ul>
+					<li><a href="index.php">Pagina Incial</a></li>
+					<li><a href="quemsomos.php">Quem Somos</a></li>
+					<li><a href="projetos.php">Projetos</a></li>
+					<li><a href="cases.php">Cases</a></li>
+					<li><a href="faleconosco.php">Fale Conosco</a></li>
+				</ul>
+
+			</div>
